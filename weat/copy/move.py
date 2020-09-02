3f6e011203726f0ecaf18a054e9aaea4f895095c
Ankitkj1999 <57802410+Ankitkj1999@users.noreply.github.com>
2020-09-02 23:05:53 +0530

    lost

---
+++ weat/copy/move.py
@@ -0,0 +1,14 @@
+import shutil
+
+shutil.move('copy\by\','copy\ty\')
+"""x=4
+if not x==3:
+ print('true')"""
+
+"""import glob
+
+if not glob.glob!=('/cf'):
+    print ('condition satisfied')
+else:
+    print('not found')
+"""
